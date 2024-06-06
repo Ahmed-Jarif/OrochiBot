af63b3d755fe5199ddc8522f620316c60a86eaae
Ahmed Jarif <ahmedjarif1230@gmail.com>
2024-06-06 12:01:26 +0600

    Update yt.js

--- scripts/cmds/yt.js
+++ scripts/cmds/yt.js
@@ -3,7 +3,7 @@ module.exports = {
     name: "youtube",
     version: "1.0",
     role: 0,
-    author: "MR SANNY",
+    author: "Ahmed Sojib",
     cooldowns: 40,
     shortdescription: "send YouTube video",
     longdescription: "",
