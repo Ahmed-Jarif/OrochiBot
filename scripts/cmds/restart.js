dabef54c2bb7197c584e66d49f8a8f7d0f3d621d
Ahmed Jarif <ahmedjarif1230@gmail.com>
2024-06-06 15:32:39 +0600

    Update restart.js

--- scripts/cmds/restart.js
+++ scripts/cmds/restart.js
@@ -27,15 +27,7 @@ module.exports = {
 		}
 	},
 
-	onLoad: function ({ api }) {
-		const pathFile = `${__dirname}/tmp/restart.txt`;
-		if (fs.existsSync(pathFile)) {
-			const [tid, time] = fs.readFileSync(pathFile, "utf-8").split(" ");
-			api.sendMessage(`✅ | Bot restarted\n⏰ | Time: ${(Date.now() - time) / 1000}s`, tid);
-			fs.unlinkSync(pathFile);
-		}
-	},
-
+	
 	onStart: async function ({ message, event, getLang }) {
 		const pathFile = `${__dirname}/tmp/restart.txt`;
 		fs.writeFileSync(pathFile, `${event.threadID} ${Date.now()}`);
